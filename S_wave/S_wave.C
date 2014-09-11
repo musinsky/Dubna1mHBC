6461700bfac0bf193e9346be67222c2edd52f0f6
Jan Musinsky <musinsky@gmail.com>
2014-09-11 14:30:42 +0200

    update S-wave

--- S_wave/S_wave.C
+++ S_wave/S_wave.C
@@ -1,42 +1,50 @@
 // Author: Jan Musinsky
-// 01/10/2009
+// 11/09/2014
 
+#include <TGraph.h>
+#include <TH1.h>
+#include <TLegend.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+
+void S_wave()
 {
-  gROOT->SetStyle("Plain");
-  g1 = new TGraph("wave2d.csv","%lg, %lg");
+  const char *fmt = "%lg %*lg %*lg %*lg %lg";
+  TGraph *g1 = new TGraph("wave2d", fmt);
   g1->SetTitle("Paris");
-  g2 = new TGraph("ba2d.csv","%lg, %lg");
+  TGraph *g2 = new TGraph("ba2d", fmt);
   g2->SetTitle("Bonn A");
-  g3 = new TGraph("bb2d.csv","%lg, %lg");
+  TGraph *g3 = new TGraph("bb2d", fmt);
   g3->SetTitle("Bonn B");
-  g4 = new TGraph("bc2d.csv","%lg, %lg");
+  TGraph *g4 = new TGraph("bc2d", fmt);
   g4->SetTitle("Bonn C");
 
   g1->Draw("AL");
-  g1->SetTitle(0);
+  g1->GetHistogram()->SetBit(TH1::kNoTitle);
   g1->SetMinimum(-0.19);
   g1->SetMaximum(1.19);
-  g1->GetXaxis()->SetRangeUser(0, 0.5);
-  g1->GetXaxis()->SetTitle("p, GeV/c");
+  g1->GetXaxis()->SetRangeUser(0.0, 0.5);
+  g1->GetXaxis()->SetTitle("p (GeV/c)");
   g1->GetYaxis()->SetTitle("u^{2} / (u^{2} + w^{2})");
-  g1->GetYaxis()->SetTitleOffset(1.2);
+  g1->GetYaxis()->SetTitleOffset(1.25);
   g1->GetYaxis()->CenterTitle();
-  g2->Draw("L"); g2->SetLineStyle(2);
-  g3->Draw("L"); g3->SetLineStyle(3);
-  g4->Draw("L"); g4->SetLineStyle(4);
+  g2->SetLineStyle(2);
+  g2->Draw("L");
+  g3->SetLineStyle(3);
+  g3->Draw("L");
+  g4->SetLineStyle(4);
+  g4->Draw("L");
 
-  leg = new TLegend(0.55,0.7,0.85,0.85);
-  leg->AddEntry(g1,"Paris","l");
-  leg->AddEntry(g2,g2->GetTitle(),"l");
-  leg->AddEntry(g3,g3->GetTitle(),"l");
-  leg->AddEntry(g4,g4->GetTitle(),"l");
-  leg->SetFillColor(0);
+  TLegend *leg = new TLegend(0.55, 0.70, 0.85, 0.85);
+  leg->AddEntry(g1, "", "L");
+  leg->AddEntry(g2, "", "L");
+  leg->AddEntry(g3, "", "L");
+  leg->AddEntry(g4, "", "L");
   leg->SetTextAlign(22);
+  leg->SetLineColor(kGray);
   leg->Draw();
 
-  gPad->SetGridx();
-  gPad->SetGridy();
-  gPad->SetWindowSize(500,500);
-  gROOT->LoadMacro("$ROOTSYS/macros/printTeX.C");
-  printTeX("S_waves");
+  gStyle->SetGridColor(kGray);
+  gPad->SetGrid();
+  gPad->GetCanvas()->SetWindowSize(500, 500);
 }
